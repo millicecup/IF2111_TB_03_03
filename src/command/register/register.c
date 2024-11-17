110139dc1de83e361f33f0e8b172d47012622445
millicecup <147978444+millicecup@users.noreply.github.com>
2024-11-17 16:03:55 +0700

    Add register

---
+++ src/command/register/register.c
@@ -0,0 +1,45 @@
+#include "../../ADT/user/user.h"
+#include "../../ADT/Mesin_Kata/mesinkata.h"
+#include "../../ADT/List/arrayuser.h"
+#include <stdio.h>
+
+
+// Register a new user
+int registerUser() {
+    User user1;
+    Word name, password;
+    char input[MAX_LEN];
+
+    // Initialize the user array
+    initializeUserArray();
+
+    // Get name input
+    printf("Enter Name: ");
+    readLine(input, MAX_LEN);
+    toWord(&name, input);
+
+    // Get password input
+    printf("Enter Password: ");
+    readLine(input, MAX_LEN);
+    toWord(&password, input);
+
+    // Check if user already exists
+    if (cariUser(&name)) {
+        printf("User already exists.\n");
+        return 0;
+    }
+
+    // Initialize and add the new user
+    initUser(&user1, &name, &password);
+    addUser(&user1);
+    
+    // Print user information
+    printf("\nUser successfully created!\n");
+    printUser(&user1);
+    
+    return 0;
+}
+
+/*
+gcc -IADT -I../command/register -I../command Mesin_Kata/mesinkata.c Mesin_Kata/mesinkarakterv2.c user/user.c List/arrayuser.c ../command/register/register.c ../command/start.c -o program
+*/
---
+++ src/command/register/register.h
@@ -0,0 +1,14 @@
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include "../../ADT/user/user.h"
+
+// Register a new user
+int registerUser();
+/*
+{ I.S. : User array terdefinisi, username dan password diberikan }
+{ F.S. : Menambahkan user baru ke array, menyimpan data ke file jika sukses }
+{ Mengembalikan 1 jika berhasil, 0 jika gagal }
+*/
+
+#endif
