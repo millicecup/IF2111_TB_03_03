ceaae8a995566a38eef7d29b1a4d2e6fd764afdd
millicecup <147978444+millicecup@users.noreply.github.com>
2024-11-17 17:26:41 +0700

    Modify user to static and combine the arrayuser to user

--- src/ADT/user/user.c
+++ src/ADT/user/user.c
@@ -1,14 +1,114 @@
 #include <stdio.h>
 #include "user.h"
 #include "../Mesin_Kata/mesinkata.h"
-#include "../List/arrayuser.h"
-#include <string.h>
 
 #define DATA_FILE "users.txt"
 
 // Global variable to store all users
-static ArrayDin userArray;
+static ArrayUser userArray;
 static boolean isInitialized = false;
+
+ArrayUser MakeArrayUser() {
+    ArrayUser array;
+    array.Neff = 0; // Start with an empty array
+    return array;
+}
+
+boolean IsEmptyUser(ArrayUser array) {
+    return (array.Neff == 0);
+}
+
+int LengthUser(ArrayUser array) {
+    return array.Neff;
+}
+
+User GetUser(ArrayUser array, int i) {
+    if (i >= 0 && i < array.Neff) {
+        return array.A[i];
+    }
+    // Handle error if the index is invalid
+    User errorUser = {{"INVALID"}, {"INVALID"}, -1};
+    return errorUser;
+}
+
+int GetUserCapacity() {
+    return MAX_USERS;
+}
+
+void InsertAtUser(ArrayUser *array, User el, int i) {
+    if (i >= 0 && i <= array->Neff && array->Neff < MAX_USERS) {
+        for (int j = array->Neff; j > i; j--) {
+            array->A[j] = array->A[j - 1];
+        }
+        array->A[i] = el;
+        array->Neff++;
+    }
+}
+
+void InsertLastUser(ArrayUser *array, User el) {
+    if (array->Neff < MAX_USERS) {
+        array->A[array->Neff] = el;
+        array->Neff++;
+    }
+}
+
+void InsertFirstUser(ArrayUser *array, User el) {
+    if (array->Neff < MAX_USERS) {
+        for (int i = array->Neff; i > 0; i--) {
+            array->A[i] = array->A[i - 1];
+        }
+        array->A[0] = el;
+        array->Neff++;
+    }
+}
+
+void DeleteAtUser(ArrayUser *array, int i) {
+    if (i >= 0 && i < array->Neff) {
+        for (int j = i; j < array->Neff - 1; j++) {
+            array->A[j] = array->A[j + 1];
+        }
+        array->Neff--;
+    }
+}
+
+void DeleteLastUser(ArrayUser *array) {
+    if (array->Neff > 0) {
+        array->Neff--;
+    }
+}
+
+void DeleteFirstUser(ArrayUser *array) {
+    if (array->Neff > 0) {
+        for (int i = 0; i < array->Neff - 1; i++) {
+            array->A[i] = array->A[i + 1];
+        }
+        array->Neff--;
+    }
+}
+
+void PrintArrayUser(ArrayUser array) {
+    for (int i = 0; i < array.Neff; i++) {
+        printf("User %d: %s, Password: %s, Money: %d\n", i, array.A[i].name, array.A[i].password, array.A[i].money);
+    }
+}
+
+ArrayUser CopyArrayUser(ArrayUser array) {
+    ArrayUser newArray;
+    newArray.Neff = array.Neff;
+    for (int i = 0; i < array.Neff; i++) {
+        newArray.A[i] = array.A[i];
+    }
+    return newArray;
+}
+
+int SearchArrayUser(ArrayUser array, Word name) {
+    for (int i = 0; i < array.Neff; i++) {
+        if (isKataEqual(&array.A[i].name, &name)) {
+            return i;  // Found the user at index i
+        }
+    }
+    return -1;  // User not found
+}
 
 void initUser(User *user, const Word *name, const Word *password) {
     user->name = *name;
@@ -26,7 +126,7 @@ void printUser(const User *user) {
 
 void initializeUserArray() {
     if (!isInitialized) {
-        userArray = MakeArrayDin();
+        userArray = MakeArrayUser();
         loadUsersFromFile();
         isInitialized = true;
     }
@@ -36,7 +136,7 @@ void addUser(const User *user) {
     if (!isInitialized) {
         initializeUserArray();
     }
-    InsertLast(&userArray, *user);
+    InsertLastUser(&userArray, *user);
     saveUsersToFile();
 }
 
@@ -44,7 +144,7 @@ boolean cariUser(const Word *name) {
     if (!isInitialized) {
         return false;
     }
-    IdxType idx = SearchArrayDin(userArray, *name);
+    IdxType idx = SearchArrayUser(userArray, *name);
     return (idx != -1);
 }
 
@@ -65,7 +165,7 @@ void loadUsersFromFile() {
         for (int i = 0; i < count; i++) {
             User user;
             fread(&user, sizeof(User), 1, file);
-            InsertLast(&userArray, user);
+            InsertLastUser(&userArray, user);
         }
         fclose(file);
     }
--- src/ADT/user/user.h
+++ src/ADT/user/user.h
@@ -3,8 +3,11 @@
 
 #include "../Mesin_Kata/mesinkata.h"
 #include "../boolean.h"
+#include "../barang/barang.h"
 
 #define MAX_LEN 50
+#define MAX_USERS 100
+#define IdxType int
 
 /* User definition */
 typedef struct {
@@ -13,6 +16,97 @@ typedef struct {
     int money;
 } User;
 
+/* Array structure for Users (static array) */
+typedef struct {
+    User A[MAX_USERS];
+    int Neff; // effective size
+} ArrayUser;
+
+
+/* Function prototypes */
+ArrayUser MakeArrayUser();
+/* 
+{ I.S. : sembarang }
+{ F.S. : Mengembalikan ArrayUser kosong dengan Neff = 0 }
+*/
+
+boolean IsEmptyUser(ArrayUser array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Mengembalikan true jika Neff = 0 }
+*/
+
+int LengthUser(ArrayUser array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Mengembalikan Neff }
+*/
+
+User GetUser(ArrayUser array, int i);
+/*
+{ I.S. : ArrayUser terdefinisi, i di antara 0..Neff-1 }
+{ F.S. : Mengembalikan elemen ke-i dari array }
+*/
+
+int GetUserCapacity();
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Mengembalikan kapasitas array, yaitu MAX_USERS }
+*/
+
+void InsertAtUser(ArrayUser *array, User el, int i);
+/*
+{ I.S. : ArrayUser terdefinisi, i di antara 0..Neff }
+{ F.S. : Menambahkan elemen el ke array pada posisi i }
+*/
+
+void InsertLastUser(ArrayUser *array, User el);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menambahkan elemen el di akhir array }
+*/
+
+void InsertFirstUser(ArrayUser *array, User el);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menambahkan elemen el di awal array }
+*/
+
+void DeleteAtUser(ArrayUser *array, int i);
+/*
+{ I.S. : ArrayUser terdefinisi, i di antara 0..Neff-1 }
+{ F.S. : Menghapus elemen ke-i dari array }
+*/
+
+void DeleteLastUser(ArrayUser *array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menghapus elemen terakhir dari array }
+*/
+
+void DeleteFirstUser(ArrayUser *array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menghapus elemen pertama dari array }
+*/
+
+void PrintArrayUser(ArrayUser array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menampilkan isi array ke layar }
+*/
+
+ArrayUser CopyArrayUser(ArrayUser array);
+/*
+{ I.S. : ArrayUser terdefinisi }
+{ F.S. : Menghasilkan salinan array }
+*/
+
+int SearchArrayUser(ArrayUser array, Word name);
+/*
+{ I.S. : ArrayUser terdefinisi, name diberikan }
+{ F.S. : Mengembalikan indeks pertama elemen dengan name tersebut, -1 jika tidak ditemukan }
+*/
 
 // User management functions
 void initUser(User *user, const Word *name, const Word *password);
@@ -56,4 +150,6 @@ void loadUsersFromFile();
 { I.S. : User array terdefinisi }
 { F.S. : User array diisi dengan data dari file users.txt }
 */
+
+
 #endif
