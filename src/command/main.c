04c1cfc7377b1a8e436b33898d88a6aa6108b7b9
millicecup <147978444+millicecup@users.noreply.github.com>
2024-11-19 10:18:03 +0700

    Modify main.c

--- src/command/main.c
+++ src/command/main.c
@@ -1,19 +1,37 @@
 #include "../../ADT/user/user.h"
 #include <stdio.h>
 #include "../../ADT/Mesin_Kata/mesinkata.h"
+#include "../../ADT/Mesin_Kata/utils.h"
+#include "../../ADT/boolean.h"
+#include "../../ADT/barang/barang.h"
 
 #include "register/register.h"
-
+#include "start/start.h"
+#include "login/login.h"
+#include "load/load.h"
 
 int main(){
-    registerUser();
+    char input[MAX_LEN];
+    Word command;
+    Word registerCommand, loginCommand;  // Declare Word variables for the commands
+    toWord(&registerCommand, "register");  // Convert "register" to Word
+    toWord(&loginCommand, "login");  // Convert "login" to Word
+    
+    printf("Enter a command : ");
+    readLine(input, MAX_LEN);
+    toWord(&command, input);  // Convert input to Word
+    
+    if (isKataEqual(&command, &registerCommand)) {  // Compare Word types
+        registerUser();
+    }
+    else if (isKataEqual(&command, &loginCommand)) {  // Compare Word types
+        login();
+    }
+    else{
+        printf("Invalid command\n");
+    }
+
     return 0;
 }
 
-/*
-/*
-kalo mau simpen di ADT :
-gcc -IADT -I../command/register -I../command Mesin_Kata/mesinkata.c Mesin_Kata/mesinkarakterv2.c user/user.c ../command/register/register.c ../command/start.c -o program
-kalo mau simpen di command register :
-gcc -Icommand -I../ADT Mesin_Kata/mesinkata.c Mesin_Kata/mesinkarakterv2.c user/user.c register/register.c main.c -o program
-*/
+// gcc -IADT -I../command/register -I../command Mesin_Kata/mesinkata.c Mesin_Kata/utils.c Mesin_Kata/mesinkarakterv2.c user/user.c ../command/register/register.c ../command/main.c ../command/login/login.c -o program
