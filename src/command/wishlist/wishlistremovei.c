c727123ea08f45f711d2332825bc2049251b7e2e
Fadhil Rifqi Rabbani Pane <163860803+Belligool@users.noreply.github.com>
2024-12-17 21:50:54 +0700

    add wishlist remove <i> semoga bener

---
+++ src/command/wishlist/wishlistremovei.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "wishlistremovei.h"
+
+void WishlistRemoveI(Wishlist *wishlist) {
+    if (IsEmptyWishlist(*wishlist)) {
+        printf("Penghapusan barang WISHLIST gagal dilakukan, WISHLIST kosong!\n");
+        return;
+    }
+
+    printf("Masukkan posisi barang yang ingin dihapus: ");
+    STARTINPUT(stdin);
+
+    boolean isValidInput = true;
+    int position = 0;
+
+    for (int i = 0; i < currentInput.Length; i++) {
+        if (currentInput.TabWord[i] < '0' || currentInput.TabWord[i] > '9') {
+            isValidInput = false;
+            break;
+        }
+        position = position * 10 + (currentInput.TabWord[i] - '0');
+    }
+
+    if (!isValidInput) {
+        printf("Penghapusan barang WISHLIST gagal dilakukan, command tidak valid!\n");
+        return;
+    }
+
+    int totalItems = NbWishlist(*wishlist);
+
+    if (position < 1 || position > totalItems) {
+        printf("Penghapusan barang WISHLIST gagal dilakukan, Barang ke-%d tidak ada di WISHLIST!\n", position);
+        return;
+    }
+
+    address prev = Nil;
+    address curr = First(*wishlist);
+    int count = 1;
+
+    while (count < position) {
+        prev = curr;
+        curr = Next(curr);
+        count++;
+    }
+
+    if (prev == Nil) {
+        DelFirst(wishlist, &curr);
+    } else {
+        DelAfter(wishlist, &curr, prev);
+    }
+
+    printf("Berhasil menghapus barang posisi ke-%d dari wishlist!\n", position);
+    Dealokasi(&curr);
+}
---
+++ src/command/wishlist/wishlistremovei.h
@@ -0,0 +1,12 @@
+#ifndef WISHLISTREMOVEI_H
+#define WISHLISTREMOVEI_H
+
+#include "../../ADT/boolean.h"
+#include "../../ADT/LinkedList/linkedlist.h"
+#include "../../ADT/Mesin_Baris/mesinbaris.h"
+#include "../../ADT/Mesin_Kata/mesinkata.h"
+#include "../../ADT/Mesin_Karakter/mesinkarakter.h"
+
+void WishlistRemoveI(Wishlist *wishlist);
+
+#endif
